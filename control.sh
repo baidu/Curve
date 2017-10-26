b26dfeb750865a9944ea06f175bfc9e70bec0525
licong08 <licong08@baidu.com>
2017-10-26 17:50:45 +0800

    fix control for mistake

--- control.sh
+++ control.sh
@@ -1,32 +1,3 @@
-#!/bin/bash
-
-if [ ! -n "$1" ] then
-    echo "Usages: bash control.sh [start|stop|reload|terminate]"
-    exit 0
-fi
-
-if [ $1 = start ] then
-    psid=`ps aux | grep "uwsgi" | grep -v "grep" | wc -l`
-    if [ $psid -gt 4 ] then
-        echo "uwsgi is running!"
-        exit 0
-    else
-        uwsgi /etc/uwsgi.ini
-        echo "Start uwsgi service [OK]"
-    fi
-elif [ $1 = stop ];then
-    killall -9 uwsgi
-    echo "Stop uwsgi service [OK]"
-elif [ $1 = restart ];then
-    killall -9 uwsgi
-    /usr/bin/uwsgi --ini /etc/uwsgi.ini
-    echo "Restart uwsgi service [OK]"
-
-else
-    echo "Usages: sh uwsgiserver.sh [start|stop|restart]"
-fi
-
-
 #! /bin/sh
 
 readonly G_HOME=`pwd`
