a7534cacd449b89b52c68c102ba0c4517160d284
licong <missingmisaka@gmail.com>
2018-02-01 16:42:53 +0800

    revert changes merged by fault

--- api/curve/app.py
+++ api/curve/app.py
@@ -34,7 +34,7 @@ def create_app():
     init app
     :return: flask app
     """
-    app = flask.Flask(__name__, static_folder=STATIC_PATH, static_url_path='')
+    app = flask.Flask(__name__, static_folder=STATIC_PATH)
     app.url_map.converters['regex'] = RegexConverter
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + SQLITE_PATH
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -44,29 +44,12 @@ def create_app():
     app.register_blueprint(bp, url_prefix='/v1')
     CORS(app)
 
-    @app.route('/swagger-ui')
-    def swagger_index(url=None):
-        """
-        redirect to index
-        :return:
-        """
-        return flask.redirect('swagger-ui/index.html')
-
-    @app.route('/swagger-ui/')
-    def swagger_index2(url=None):
-        """
-        redirect to index
-        :return:
-        """
-        return flask.redirect('swagger-ui')
-
-    @app.route('/')
+    @app.route('/<regex("$"):url>')
     def index(url=None):
         """
         redirect to index
         :return:
         """
-        flask.head
-        return app.send_static_file('index.html')
+        return flask.redirect(INDEX_PAGE)
 
     return app
