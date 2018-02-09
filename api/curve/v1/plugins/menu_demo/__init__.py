03988a1d85ff58f71109d933e8e86e1d6df04100
licong <missingmisaka@gmail.com>
2018-02-09 11:39:35 +0800

    在 alpha 版中合入插件

---
+++ api/curve/v1/plugins/menu_demo/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+    plugin
+    ~~~~
+    plugin demo for menu
+    :entity: menus()
+    :invoked: init web page
+    :exclusive: false
+    :entity: ${menu_item}()
+    :invoked: menu item is clicked
+    :exclusive: true
+    :copyright: (c) 2017 by Baidu, Inc.
+    :license: Apache, see LICENSE for more details.
+"""
+
+
+def menus():
+    """
+    :return: [(action, menu_name)]
+    """
+    return [
+        ('cancel_label', 'Cancel the label')
+    ]
+
+
+def cancel_label(api, start_time, end_time):
+    """
+    :func action: action in menus
+    :param api: api for plugins
+    :param start_time:
+    :param end_time:
+    :return:
+    """
+    api.set_label(start_time, end_time, api.LABEL_ENUM.normal)
+    return ''
---
+++ api/curve/v1/plugins/ref_area_demo/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+    plugin
+    ~~~~
+    plugin demo for ref
+    :entity: reference()
+    :invoked: trend rendering
+    :exclusive: false
+    :copyright: (c) 2017 by Baidu, Inc.
+    :license: Apache, see LICENSE for more details.
+"""
+
+
+def reference(api, line):
+    """
+    :param api: api for plugins
+    :param line: [(timestamp, value...)]
+    :return: [(timestamp, y_lower, y_upper)]
+    """
+    result = []
+
+    if len(line) > 0:
+        start_time = line[0][0]
+        end_time = line[-1][0]
+
+        line = api.get_data(start_time - api.WEEK, end_time - api.WEEK)
+        area = []
+        for timestamp, value in line:
+            if value is not None:
+                area.append((timestamp, value * 0.9, value * 1.1))
+            else:
+                area.append((timestamp, None, None))
+        result.append(area)
+
+    return 'Week on Week 10% diff', result
